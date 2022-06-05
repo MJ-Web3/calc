fe81014803dbe95e2d93eeea74ecff891434785a
MJ-Web3 <103333893+MJ-Web3@users.noreply.github.com>
2022-06-05 16:52:26 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,28 +6,24 @@ btn.forEach((button)=>{
     button.addEventListener('click',(x)=>{
         
         if(x.target.dataset.btn == 'clear-all'){
-
             box.value = '';
         }
         else if(x.target.dataset.btn == 'c'){
-
             box.value = box.value.slice(0,-1);
-
         }else if(x.target.dataset.btn != '='){
-
             box.value += x.target.dataset.btn;
-
         }else if(x.target.dataset.btn == '='){
-
-            let val = eval(box.value);
-            box.value = '';
-            box.value = val;
-
+            if(box.value == ""){
+                box.value = '';
+            }else{
+                let val = eval(box.value);
+                box.value = '';
+                box.value = val;
+            }
         }else{
             box.value = '';
             box.value = 'ERROR';
         }
-
     });
 })
 
